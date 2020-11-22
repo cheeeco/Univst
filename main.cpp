67e3ab917a707fa3bf4c2c52d4519751f55c1f11
cheeeco <71634483+cheeeco@users.noreply.github.com>
2020-11-22 03:38:39 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 
 #include "univst.h"
 
-/*
- * Забавный баг - когда в вектор campus добавляют вторую и более общагу, создаются и уничтожаются копии предыдущих.
-*/
 int main()
 {
     University MIPT("Phystech", "Dolgoprudniy, Institutskiy per., 9");
@@ -16,6 +13,10 @@ int main()
     
     MIPT.build("8ka", 300);
     
+    std::cout << "There are " << MIPT.getCapacity() << " students in " << MIPT.getName() << "." << std::endl;
+
+    MIPT.build("1ka", 228);
+
     std::cout << "There are " << MIPT.getCapacity() << " students in " << MIPT.getName() << "." << std::endl;
 
     std::cout << MIPT.getName() << " is in " << &MIPT << "." << std::endl;
